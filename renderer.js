e0bf501ffa956cadef0e3d2e7a2e9d3ba2114c7e
Paranat Klinhom <tl.topspin.48@gmail.com>
2019-08-31 15:24:44 +0700

    feature/ add socket.io log server

--- renderer.js
+++ renderer.js
@@ -4,22 +4,49 @@
 
 const openBtn = document.getElementById('openBtn');
 const closeServer = document.getElementById('closeServer');
+const fixedPort = document.getElementById('getport');
+
 let path = require('path')
-// var spawn = require('electron-spawn')
 var spawn = require('child_process').spawn
+let log = require('electron-log')
+var app = require('express')();
+var http = require('http').Server(app)
+var io = require('socket.io')(http);
+require('events').EventEmitter.prototype._maxListeners = 0;
+
+http.listen(1000, function() {
+    console.log("socket start : 1000")
+})
+
+io.on('connection', client => {
+    console.log('Client connected..');
+    client.on('join', function(data) {
+        log.info(data)
+    })        
+})
+
+
 var ls = null
+var scriptOutput = "";
+
+
 openBtn.addEventListener('click',function (event) {
     var webServerDirectory = path.join(__dirname, 'resources', 'app', 'app.js');
     console.log('starting node script: ' + webServerDirectory);
     
-    ls = spawn('node', [webServerDirectory]);
+    ls = spawn('node',[webServerDirectory,'DEBUG=express:*']);
 
     ls.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
+        console.log(`stdoutDATANAJA: ${data}`);
+        data = data.toString();
     });
 
     ls.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
+        // console.log(`stderr: ${data}`);
+        // data = data.toString();
+
+        io.sockets.emit('new-message', `status ===> ${data} \n`)
+
     });
 
     ls.on('close', (code) => {
@@ -34,7 +61,6 @@ openBtn.addEventListener('click',function (event) {
 })
 
 closeServer.addEventListener('click', function(event) {
-    console.log("aaa")
     ls.kill();
 
     document.getElementById('status').innerHTML = "ระบบสิ้นสุดการทำงานแล้ว"
@@ -43,5 +69,5 @@ closeServer.addEventListener('click', function(event) {
     let myNotification = new Notification('Server is Closed', {
         body: 'ระบบกำลังปิดตัวลง'
     })
+    
 })
-
